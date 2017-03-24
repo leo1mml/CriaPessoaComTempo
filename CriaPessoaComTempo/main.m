2c340e5d013454282f0e0b3595ed3eb2032fa57d
Leonel Menezes <leonelmenezes@mac325.local>
2017-03-24 15:40:19 -0300

    Initial Commit

---
+++ CriaPessoaComTempo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CriaPessoaComTempo
+//
+//  Created by Leonel Menezes on 24/03/17.
+//  Copyright © 2017 BEPID. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
